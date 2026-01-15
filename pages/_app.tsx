d72ab56ec319f470fdbd12a2871ef0dfa601471d
Dandy Candra <dandycandrapratama@gmail.com>
2026-01-15 11:06:35 +0700

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import 'moment/locale/id';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/config/queryClient';
+import moment from 'moment';
+
+// Declare moment with "indonesia" locale
+moment.locale('id');
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>FE TEST JASAMARGA</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
---
+++ types/client.types.ts
@@ -0,0 +1,58 @@
+import { AxiosRequestConfig } from 'axios';
+
+export type MutationMethodType = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+export interface FetchOptions {
+  url: string;
+  inputParams?: { [key: string]: string | number };
+}
+
+export interface MutationDataOptions {
+  url: string;
+  method: MutationMethodType;
+  body?: { [key: string]: any };
+  baseURL?: string;
+  inputParams?: { [key: string]: any };
+  config?: AxiosRequestConfig;
+}
+
+export interface MutationFormDataOptions {
+  url: string;
+  method: MutationMethodType;
+  body: { [key: string]: any };
+  baseURL?: string;
+  inputParams?: { [key: string]: any };
+  config?: AxiosRequestConfig;
+}
+
+export interface ApiError {
+  response: {
+    data: {
+      message: string;
+      status: number;
+    };
+  };
+}
+
+export interface MetaType {
+  isFirstPage: boolean;
+  isLastPage: boolean;
+  currentPage: number;
+  previousPage: null | number;
+  nextPage: null | number;
+  pageCount: number;
+  totalCount: number;
+}
+
+export interface SuccessResponse<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+export interface SuccessResponsePagination<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+  meta: MetaType;
+}
